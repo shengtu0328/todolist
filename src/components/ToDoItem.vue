786f6f571037b36ae80b50c4d8941c0cc4849b36
rqxiao <1007689622@qq>
2019-02-20 11:22:33 +0800

    todolist

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+<li class="item" @click="handleclick">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods:{
+    handleclick:function () {
+      this.$emit('delete',this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.item {
+  color:green;
+}
+</style>
---
+++ src/todolist.vue
@@ -0,0 +1,43 @@
+<template>
+  <div >
+    <input class="item" v-model="inputValue"/>
+    <button @click="handleSubmit">提交</button>
+    <ul>
+      <todo-item v-for="(item, index) of list"
+                 :key="index"
+                 :content="item"
+                 :index="index"
+                 @delete="handleDelete"
+      ></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue'
+
+export default {
+  components: {
+    'todo-item': ToDoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue) // this.list其实就是this.$data.list
+      this.inputValue = ''
+    },
+    handleDelete:function(index){
+      this.list.splice(index,1)
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+</style>
